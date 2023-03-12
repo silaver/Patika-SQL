a7d1649416f8479167cc8afa5489fb2fcbeed791
Sıla Er <98685874+silaver@users.noreply.github.com>
2023-03-12 23:44:26 +0300

    Add files via upload
    Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.

---
+++ SQLODEV4.sql
@@ -0,0 +1,14 @@
+--film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklý deðerleri sýralayýnýz.
+SELECT DISTINCT replacement_cost FROM film;
+
+--film tablosunda bulunan replacement_cost sütununda birbirinden farklý kaç tane veri vardýr?
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+--film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile baþlar ve ayný zamanda rating 'G' ye eþittir?
+SELECT COUNT(*) FROM film WHERE (title LIKE (%T)) AND (rating = 'G')
+
+--country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluþmaktadýr?
+SELECT COUNT(*) FROM country WHERE (length = 5) AND (name LIKE ('_____'));
+
+--city tablosundaki þehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+SELECT COUNT(*) FROM city WHERE name ILIKE (%r);
