a658eab658d113314616441b002eed568cf64131
Sıla Er <98685874+silaver@users.noreply.github.com>
2023-03-13 23:34:05 +0300

    Add files via upload
    Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.

---
+++ SQLODEV7.sql
@@ -0,0 +1,13 @@
+--film tablosunda bulunan filmleri rating deðerlerine göre gruplayýnýz.
+SELECT * FROM film GROUP BY rating
+
+--film tablosunda bulunan filmleri replacement_cost sütununa göre grupladýðýmýzda 
+--film sayýsý 50 den fazla olan replacement_cost deðerini ve karþýlýk gelen film sayýsýný sýralayýnýz.
+SELECT replacement_cost, COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*) > 50
+
+--customer tablosunda bulunan store_id deðerlerine karþýlýk gelen müþteri sayýlarýný nelerdir? 
+SELECT COUNT(*), store_id FROM customer GROUP BY store_id
+
+--city tablosunda bulunan þehir verilerini country_id sütununa göre gruplandýrdýktan sonra 
+--en fazla þehir sayýsý barýndýran country_id bilgisini ve þehir sayýsýný paylaþýnýz.
+SELECT country_id, COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC LIMIT 1
